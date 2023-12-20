b9190f4d3382aa79249d4263f2c545d9282751cd
amyfrancis97 <amyfrancis1997@hotmail.com>
2023-12-20 15:50:02 +0000

    script for downloading required alphafold files

---
+++ FG10_alpha_fold/download_cif.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script for downloading the Alpha Fold CIF files
+
+wget https://ftp.ebi.ac.uk/pub/databases/alphafold/latest/UP000005640_9606_HUMAN_v4.tar
+tar -xf UP000005640_9606_HUMAN_v4.tar
