b158084ffb01b7cad455bfdb3159e62087353d52
amyfrancis97 <amyfrancis1997@hotmail.com>
2023-12-22 11:15:10 +0000

    various updates

--- FG10_alpha_fold/get_alpha_fold.py
+++ FG10_alpha_fold/get_alpha_fold.py
@@ -39,30 +39,29 @@ def getUniprotIDs(vep_dataset):
     df3 = df3.drop(2, axis=1)
     df3.columns = ["chrom", "pos", "ref_allele", "alt_allele", "R", "driver_stat", "gene", "protein_position"]
     df3["uniprot_conversion"] = np.nan
-
+    print(len(df3))
     # Filter rows where protein_position is not empty, "None", or NaN
     df3 = df3[df3["protein_position"].isin(["", "None", np.nan]) == False].reset_index(drop=True)
-
+    print(len(df3))
     # Extract unique genes for UniProt ID conversion
     genes = df3["gene"].unique().tolist()
-
+    print(len(genes))
     # Convert the list of genes to a string with curly braces
     genes_str = "{" + ", ".join(f'"{gene}"' for gene in genes) + "}"
-    print(genes_str)
     # Retrieve UniProt IDs using IdMappingClient
     try:
         request = IdMappingClient.submit(source="GeneCards", dest="UniProtKB", ids={genes_str})
-        time.sleep(1)
+        time.sleep(10)
         uniprotIDs = pd.DataFrame(list(request.each_result()))
         uniprotIDs.columns = ["gene", "uniprot_res"]
     except:
         print("Error in uniprot mapping")
         uniprotIDs = pd.DataFrame(columns=["gene", "uniprot_res"])
-
+    print(len(uniprotIDs))
     # Return a tuple with UniProt IDs and the processed DataFrame
     return uniprotIDs, df3
 
-def get_alpha_fold_atom(uniprot_id):
+def get_alpha_fold_atom(uniprot_id, df):
     """
     Get AlphaFold Atom Information for a UniProt ID
 
@@ -82,7 +81,7 @@ def get_alpha_fold_atom(uniprot_id):
     try:
         # Select rows from DataFrame where "uniprot_res" column matches the provided uniprot_id
         res = df[df["uniprot_res"] == uniprot_id].reset_index(drop=True)
-
+        
         # Construct the path to the CIF file based on the uniprot_id
         cif_file_path = f"{alpha_fold_cif_location}/AF-{uniprot_id}-F1-model_v4.cif.gz"
         
@@ -98,9 +97,9 @@ def get_alpha_fold_atom(uniprot_id):
             filtered_cif = cif_file[atom_mask][0]  # Extract column 0 from filtered rows
             position_mask = filtered_cif[filtered_cif.str.contains(res.loc[0, "protein_position"])]  # Select rows with protein position
             results = position_mask.str.split("?", expand=True)[1].to_string()  # Split and select the relevant column
-            results = pd.DataFrame(" ".join(results.split()).split()).transpose().iloc[:, 1:6]
-            results = pd.concat([res[["chrom", "pos", "ref_allele", "alt_allele"]], results], axis=1)
-
+            results = pd.DataFrame(" ".join(results.split()).split()).transpose().iloc[:, 1:6].reset_index(drop = True)
+            res_protein_pos = res[res["protein_position"] == position].reset_index(drop = True)
+            results = pd.concat([res_protein_pos[["chrom", "pos", "ref_allele", "alt_allele"]], results], axis=1)
         else:
             # If there are multiple rows with different protein positions
             results = []
@@ -109,24 +108,19 @@ def get_alpha_fold_atom(uniprot_id):
                 filtered_cif = cif_file[atom_mask][0]
                 position_mask = filtered_cif[filtered_cif.str.contains(position)]
                 cif_res = position_mask.str.split("?", expand=True)[1].to_string()
-                cif_res = pd.DataFrame(" ".join(cif_res.split()).split()).transpose().iloc[:, 1:6]
-                cif_res = pd.concat([res[["chrom", "pos", "ref_allele", "alt_allele"]], cif_res], axis=1)
+                cif_res = pd.DataFrame(" ".join(cif_res.split()).split()).transpose().iloc[:, 1:6].reset_index(drop = True)
+                res_protein_pos = res[res["protein_position"] == position].reset_index(drop = True)
+                cif_res = pd.concat([res_protein_pos[["chrom", "pos", "ref_allele", "alt_allele"]], cif_res], axis=1)
                 results.append(cif_res)
 
             # Concatenate the results from multiple positions and drop any NaN values
-            results = pd.concat(results).dropna()
-        
+            results = pd.concat(results)
+        print(len(results))
         results = results.rename(columns = {1: "X_coordinate", 2: "Y_coordinate", 3: "Z_coordinate", 4: "atom_sit_occupancy", 5: "isotropic_temperature"})
-
         # Return the final DataFrame containing the results
         return results
-
-    except FileNotFoundError:
-        print(f"CIF file not found for {uniprot_id}")
-    except pd.errors.EmptyDataError:
-        print(f"No data in CIF file for {uniprot_id}")
-    except Exception as e:
-        print(f"An unexpected error occurred for {uniprot_id}: {e}")
+    except:
+        x="y"
 
 def get_alpha_fold_struct(uniprot_id):
     """
@@ -177,11 +171,9 @@ def get_alpha_fold_struct(uniprot_id):
 
             filtered_cif = filtered_cif.loc[filtered_cif[2] == res.loc[0, "protein_position"], [6, 13]].reset_index(drop = True)
 
-            #.loc[:, [6, 13]]
-
             filtered_cif.columns = ["struct_conf_type", "struct_conf_type_ID"]
 
-            results = pd.concat([res[["chrom", "pos", "ref_allele", "alt_allele"]], filtered_cif], axis=1)
+            results = pd.concat([res[["chrom", "pos", "ref_allele", "alt_allele"]].reset_index(drop = True), filtered_cif.reset_index(drop = True)], axis=1)
 
         else:
             # If there are multiple rows with different protein positions
@@ -198,11 +190,10 @@ def get_alpha_fold_struct(uniprot_id):
                 filtered_cif = filtered_cif[filtered_cif.str.contains(position)]
 
                 filtered_cif = filtered_cif.str.split(expand = True).dropna()
-
                 filtered_cif = filtered_cif.loc[filtered_cif[2] == position, [6, 13]].reset_index(drop = True)
 
                 filtered_cif.columns = ["struct_conf_type", "struct_conf_type_ID"]
-                results = pd.concat([res[["chrom", "pos", "ref_allele", "alt_allele"]], filtered_cif], axis=1)
+                results = pd.concat([res[["chrom", "pos", "ref_allele", "alt_allele"]].reset_index(drop = True), filtered_cif.reset_index(drop = True)], axis=1)
 
             # Concatenate the results from multiple positions and drop any NaN values
             results = pd.concat(results).dropna()
@@ -210,13 +201,8 @@ def get_alpha_fold_struct(uniprot_id):
         # Return the final DataFrame containing the results
         return results.dropna()
 
-    except FileNotFoundError:
-        print(f"CIF file not found for {uniprot_id}")
-    except pd.errors.EmptyDataError:
-        print(f"No data in CIF file for {uniprot_id}")
-    except Exception as e:
-        print(f"An unexpected error occurred for {uniprot_id}: {e}")
-
+    except:
+        x="y"
 
 if __name__ == "__main__":
     variantDir = sys.argv[1]
@@ -227,14 +213,14 @@ if __name__ == "__main__":
 
     uniprotIDs, df3 = getUniprotIDs(dataset)
 
-    df = pd.merge(df3, uniprotIDs, on = "gene", how = "right").drop("uniprot_conversion", axis =1)
-
-    all_alphafold = [get_alpha_fold_atom(uniprot_id) for uniprot_id in df["uniprot_res"].unique()]
+    df = pd.merge(df3, uniprotIDs, on = "gene", how = "left").drop("uniprot_conversion", axis =1)
+    print(len(df))
+    print(df.head())
+    all_alphafold = [get_alpha_fold_atom(uniprot_id, df) for uniprot_id in df["uniprot_res"].unique()]
     all_alphafold = pd.concat(all_alphafold)
 
     struct_alphafold = [get_alpha_fold_struct(uniprot_id) for uniprot_id in df["uniprot_res"].unique()]
     struct_alphafold = pd.concat(struct_alphafold)
-
 
     # Get one hot encoding of columns B
     struct_conf_type = pd.get_dummies(struct_alphafold['struct_conf_type'])
@@ -247,6 +233,6 @@ if __name__ == "__main__":
 
     # Join the encoded df
     struct_alphafold = pd.concat([struct_alphafold, struct_conf_type, struct_conf_type_ID], axis = 1)
-
+    print(sys.argv[3])
     all_alphafold.to_csv(sys.argv[3] + "alphafold_3D_coordinates.bed", sep = "\t", index = None)
     struct_alphafold.to_csv(sys.argv[3] + "alphafold_structural.bed", sep = "\t", index = None)
